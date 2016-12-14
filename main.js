30b4197a0d00537137bf187d00c68a1292da4a31
Baokhoa Vu <tealkrysta92@gmail.com>
2016-12-14 12:45:18 -0800

    fixing stuff logic wise on project and more

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ var makeaDeck = function() {
   var A = 1
   var numCards = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
   var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-  var Hearts = value.map(function(value) {
-    return value + ' of Hearts';
+  var Hearts = value.map(function(num) {
+    return num + ' of Hearts';
   })
   var Diamonds = value.map(function(num) {
     return num + ' of Diamonds';
@@ -37,8 +37,27 @@ var makeaDeck = function() {
   var deck = Hearts.concat(Clubs, Diamonds, Spades)
 }
 
-var shuffleandSetup = function() {
-  .shuffle(deck); // Have not downloaded jQuery yet. This is the shuffle command
+var shuffle = function(deck) {
+  // .shuffle(deck); Have not downloaded jQuery yet. This is the shuffle command
+
+  // next card function
+  var currentIndex = deck.length, tempCard, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex); //2
+    currentIndex -= 1 // 3
+
+    tempCard = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = tempCard;
+  }
+  return deck;
+}
+
+var shuffleandSetup = function(deck) {
+
+  deck = shuffle(deck);
+
   for (var i = 0; i < deck.length; i++) {
     if ( i % 2 === 0){
       deckA.push(deck[i]);
@@ -59,23 +78,25 @@ var eachRound = function(cardA, cardB) {
     scoreB + 1;
     deckB.push(cardA);
     deckB.push(cardB);
-  } else
-  function war () {
+  } else {
+    // this is war
     potA = [];
-    potA.push(cardA);
-    potA.push(deckA);
-    handA.push(deckA);
+    // potA.push(cardA);
+    // draw card from deckA
+    // potA.push(drawn card);
+    // draw card from deckA
     potB = [];
-    potB.push(cardB);
-    potB.push(deckB);
-    handB.push(deckB);
-      if (handA > handB) {
+    // potB.push(cardB);
+    // draw a card from deckB
+    // potB.push(drawn card);
+    // draw card from deckB
+    potA[2] > potB[2]
         scoreA + 1;
-        deckA = potA.concat(PotB);
-      } else (handA < handB) {
+        deckA.push potA.concat(PotB);
+      } else {
         scoreB + 1;
         deckB = potA.concat(PotB);
-      }
+    }
   }
 }
 
